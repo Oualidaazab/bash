c108e003744ec349d1a0f41dbfe2b34184fc67fc
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2025-05-21 14:35:09 +0200

    Update Bash1.sh

--- Bash1.sh
+++ Bash1.sh
@@ -1,4 +1,6 @@
 #!bin/bash 
+# task is to use for loops to display only odd natural numbers from 1 to 99  .
+#from Hacker rank 
 for ((i=0; i<101; i++)); do  
   if (( i % 2 != 0 )); then 
     echo "$i"
