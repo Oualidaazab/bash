e90b0530fc69102d4c63ada7f9729ca8a7580ab5
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2026-02-17 10:27:04 +0100

    Update for__loop.sh

--- for__loop.sh
+++ for__loop.sh
@@ -1,3 +1,9 @@
+#----------------------------------------------------- 
+# HACKERRANK CHALLENGE  
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem?isFullScreen=true  
+# Use a for loop to display the natural numbers from  1 to 50 .
+#----------------------------------------------------
+
 #! /bin/bash 
 for(( i=1 ; i<51 ; i++ ));do 
      echo "$i" 
